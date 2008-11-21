de2f83ee501102e82be494faf1873468b4c6d825
Ron Hitchens <ron.hitchens@marklogic.com>
2008-11-21 02:59:12 +0000

    Checkpoint
    git-svn-id: http://developer.marklogic.com/svn/mljcr/trunk@1096 e04f4502-82db-0310-b1af-f799f365da79

---
+++ mljcr-1.3/src/java/com/marklogic/jcr/query/MarkLogicSearchIndex13.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2008,  Mark Logic Corporation. All Rights Reserved.
+ */
+
+package com.marklogic.jcr.query;
+
+import com.marklogic.jcr.compat.PMAdapter13;
+
+import org.apache.jackrabbit.core.query.ExecutableQuery;
+import org.apache.jackrabbit.core.query.QueryRootNode;
+import org.apache.jackrabbit.core.query.QueryParser;
+import org.apache.jackrabbit.core.SessionImpl;
+import org.apache.jackrabbit.core.ItemManager;
+
+import javax.jcr.query.InvalidQueryException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ron
+ * Date: Nov 20, 2008
+ * Time: 1:07:06 PM
+ */
+public class MarkLogicSearchIndex13 extends MarkLogicSearchIndex
+{
+	public ExecutableQuery createExecutableQuery (SessionImpl session,
+		ItemManager itemMgr, String statement, String language)
+		throws InvalidQueryException
+	{
+		log.info ("lang=" + language + ", stmt=" + statement);
+
+		QueryRootNode root = QueryParser.parse (statement, language,
+			session.getNamespaceResolver());
+
+		return new MLExecutableQuery (session, itemMgr, statement, language, root);
+	}
+}
